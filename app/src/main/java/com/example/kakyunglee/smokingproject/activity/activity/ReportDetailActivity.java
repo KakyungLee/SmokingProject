d31880a61eb0a9b825b88b68c30d617f09e91946
KakyungLee <eeww95@gmail.com>
2017-10-23 13:52:22 +0900

    gallery 이미지 불러오기 , camera 이미지 불러오기(error), dto 추가

--- app/src/main/java/com/example/kakyunglee/smokingproject/activity/activity/ReportDetailActivity.java
+++ app/src/main/java/com/example/kakyunglee/smokingproject/activity/activity/ReportDetailActivity.java
@@ -1,8 +1,14 @@
 package com.example.kakyunglee.smokingproject.activity.activity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.Spinner;
 
 import com.example.kakyunglee.smokingproject.R;
@@ -12,6 +18,12 @@ import com.example.kakyunglee.smokingproject.R;
  */
 
 public class ReportDetailActivity extends AppCompatActivity{
+
+    private final int PICK_FROM_CAMERA =0;
+    private final int PICK_FROM_ALBUM =1;
+    private final int CROP_FROM_IMAGE= 2;
+
+    private Uri mImageCaptureUir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +37,46 @@ public class ReportDetailActivity extends AppCompatActivity{
         ArrayAdapter adapter = ArrayAdapter.createFromResource(this,R.array.report_detail,android.R.layout.simple_spinner_dropdown_item);
         spinner.setDropDownVerticalOffset(120);
         spinner.setAdapter(adapter);
+
+        ImageButton btnGallery = (ImageButton)findViewById(R.id.open_gallery);
+        btnGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGallery();
+                Snackbar.make(v,"get image from gallery", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        ImageButton btnCamera = (ImageButton)findViewById(R.id.open_camera);
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCamera();
+                Snackbar.make(v,"get image from camera", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
+
+    public  void openCamera()
+    {
+        // 카메라 터짐..
+        /*
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        String url = "temp_"+ String.valueOf(System.currentTimeMillis())+".jpg";
+        mImageCaptureUir = Uri.fromFile(new File(Environment.getExternalStorageDirectory(),url));
+        intent.putExtra(MediaStore.EXTRA_OUTPUT,mImageCaptureUir);
+        startActivityForResult(intent,PICK_FROM_CAMERA);
+        */
+    }
+    public  void openGallery()
+    {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+        startActivityForResult(intent,PICK_FROM_ALBUM);
+    }
+
+
+
 }
---
+++ app/src/main/java/com/example/kakyunglee/smokingproject/activity/dto/QuestionDTO.java
@@ -0,0 +1,42 @@
+package com.example.kakyunglee.smokingproject.activity.dto;
+
+// 향후 추가될 가능성이 있음 > db구조와 맞춤 1022
+public class QuestionDTO {
+	public int id;
+	public int report_category_id;
+	public String email;
+	public String contents;
+	public String image_url;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getReport_category_id() {
+		return report_category_id;
+	}
+	public void setReport_category_id(int report_category_id) {
+		this.report_category_id = report_category_id;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getContents() {
+		return contents;
+	}
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+	public String getImage_url() {
+		return image_url;
+	}
+	public void setImage_url(String image_url) {
+		this.image_url = image_url;
+	}
+	
+	
+}
