ee07a3c1fa8abffef4bc5ad4e3c6c19cd411aba0
KakyungLee <eeww95@gmail.com>
2017-10-28 13:21:57 +0900

    notice detail notice 완료

--- app/src/main/java/com/example/kakyunglee/smokingproject/activity/activity/NoticeDetailActivity.java
+++ app/src/main/java/com/example/kakyunglee/smokingproject/activity/activity/NoticeDetailActivity.java
@@ -2,6 +2,8 @@ package com.example.kakyunglee.smokingproject.activity.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 import com.example.kakyunglee.smokingproject.R;
@@ -11,7 +13,7 @@ import com.example.kakyunglee.smokingproject.activity.dto.NoticeDTO;
  * Created by KakyungLee on 2017-10-23.
  */
 
-public class NoticeDetailActivity extends AppInfoActivity{
+public class NoticeDetailActivity extends AppCompatActivity{
 
 
     NoticeDTO notice;
@@ -23,10 +25,16 @@ public class NoticeDetailActivity extends AppInfoActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.notice_detail);
+
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.custom_bar);
+        TextView barTitle = (TextView)findViewById(R.id.title_bar);
+        barTitle.setText("공지사항");
+
         Intent intent = getIntent();
         notice = (NoticeDTO) intent.getExtras().getSerializable("notice_detail");
 
-        setTitle("공지사항");
+
 
         title = (TextView)findViewById(R.id.notice_title);
         created_at = (TextView)findViewById(R.id.notice_created_at);
