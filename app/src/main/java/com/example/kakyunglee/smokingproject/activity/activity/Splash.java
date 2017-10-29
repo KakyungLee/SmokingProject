f2104aabfb8d616eab73c95f41135282662eeb4d
KakyungLee <eeww95@gmail.com>
2017-10-30 02:19:04 +0900

    premission complete and change icon and title

--- app/src/main/java/com/example/kakyunglee/smokingproject/activity/activity/Splash.java
+++ app/src/main/java/com/example/kakyunglee/smokingproject/activity/activity/Splash.java
@@ -2,15 +2,16 @@ package com.example.kakyunglee.smokingproject.activity.activity;
 
 import android.Manifest;
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.os.AsyncTask;
-import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.ActivityCompat;
 
 import com.example.kakyunglee.smokingproject.R;
+import com.example.kakyunglee.smokingproject.activity.util.UtilPermissions;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by ckj on 2017-10-22.
@@ -18,109 +19,58 @@ import com.example.kakyunglee.smokingproject.R;
 
 public class Splash extends Activity {
 
-    int check =0;
+    private static final int PERMISSION_ALL = 0;
+    private Handler h;
+    private Runnable r;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
-        new MyAsyncTask().execute();
 
+        h = new Handler();
+        r = new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(Splash.this, MainActivity.class));
+                finish();
+            }
+        };
+
+        String[] PERMISSIONS = {
+                android.Manifest.permission.ACCESS_FINE_LOCATION,
+                android.Manifest.permission.CAMERA,
+                android.Manifest.permission.READ_EXTERNAL_STORAGE
+        };
+
+        if(!UtilPermissions.hasPermissions(this, PERMISSIONS)){
+            ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSION_ALL);
+        }
+        else
+            h.postDelayed(r, 1500);
     }
 
-        public void makePermissionDialog(final String permissionType){
-            AlertDialog.Builder dialog = new AlertDialog.Builder(Splash.this);
-            dialog.setTitle("권한이 필요합니다.")
-                    .setMessage("권한을 허용하지 않는다면 정상적인 앱 사용이 불가능 합니다. 권한을 허용하시겠습니까?")
-                    .setPositiveButton("네", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                                requestPermissions(new String[]{permissionType}, 1000); } } })
-                    .setNegativeButton("아니요", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                        }
-                    })
-                    .create()
-                    .show();
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+
+        int index = 0;
+        Map<String, Integer> PermissionsMap = new HashMap<String, Integer>();
+        for (String permission : permissions){
+            PermissionsMap.put(permission, grantResults[index]);
+            index++;
         }
 
-        public boolean getPermission(){
-            // 퍼미션
-            if(Build.VERSION.SDK_INT >= 23) {
-                String[] permissions = {
-                        Manifest.permission.ACCESS_FINE_LOCATION,
-                        Manifest.permission.CAMERA,
-                        android.Manifest.permission.READ_EXTERNAL_STORAGE
-                };
-
-                int[] permissionResult = {
-                        checkSelfPermission(permissions[0]),
-                        checkSelfPermission(permissions[1]),
-                        checkSelfPermission(permissions[2])
-                };
-
-                if (permissionResult[0] == PackageManager.PERMISSION_DENIED) { // 위치 권한 설정
-
-                    if(shouldShowRequestPermissionRationale(permissions[0])){ // 거부한적 있는 경우
-                        makePermissionDialog(permissions[0]); // 권한 설정 재요청
-                    }
-                    else{
-                        requestPermissions(new String[]{permissions[0]}, 1000); // 처음 권한 요청하는 경우
-                    }
-
-                }
-                if (permissionResult[1] == PackageManager.PERMISSION_DENIED) { // 카메라 권한 설정
-                    if(shouldShowRequestPermissionRationale(permissions[1])){ // 거부한적 있는 경우
-                        makePermissionDialog(permissions[1]); // 권한 설정 재요청
-                    }
-                    else{
-                        requestPermissions(new String[]{permissions[1]}, 1000); //처음 권한 요청하는 경우
-                    }
-                }
-                if (permissionResult[2] == PackageManager.PERMISSION_DENIED) { // 갤러리 권한 설정정
-                    if(shouldShowRequestPermissionRationale(permissions[2])){ //거부한적 있는 경우
-                        makePermissionDialog(permissions[2]); // 권한 설정 재용청
-                    }
-                    else{
-                        requestPermissions(new String[]{permissions[2]}, 1000); // 처음 권한 요청하는 경우
-                    }
-                }
-            }
-
-            return true;
+        if((PermissionsMap.get( android.Manifest.permission.ACCESS_FINE_LOCATION) != 0)
+                || PermissionsMap.get( Manifest.permission.CAMERA) != 0
+                || PermissionsMap.get( Manifest.permission.READ_EXTERNAL_STORAGE) != 0){
+            finish();
         }
-
-        class MyAsyncTask extends AsyncTask<Void, Void, Void>{
-
-            boolean isTrue = false;
-
-            @Override
-            protected void onPreExecute(){
-                super.onPreExecute();
-                //포그라운드에서 처리할때 사용
-                isTrue = getPermission();
-
-
-            }
-
-            protected Void doInBackground(Void... params) {
-               // 백그라운드에서 처리 할때 사용
-                return null;
-
-            }
-
-            protected  void onPostExecute(Void result){
-               super.onPostExecute(result);
-                // 퍼미션 완료후 실행할 작업 선택
-                    Intent mainIntent = new Intent(Splash.this, MainActivity.class);
-                    Splash.this.startActivity(mainIntent);
-                    Splash.this.finish();
-
-            }
-
+        else
+        {
+            h.postDelayed(r, 1500);
         }
+    }
+
 
 }
 
